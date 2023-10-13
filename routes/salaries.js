2c12a32e06d3763849ef58ab5ca146c071f36c2c
Maheedeen Jormae <maheedeen@gmail.com>
2023-10-13 21:51:49 +0700

    add report salary

--- routes/salaries.js
+++ routes/salaries.js
@@ -98,21 +98,50 @@ router.get("/date/:date", async (req, res) => {
     return res.status(500).send();
   }
 });
+
+router.get("/report/:date", async (req, res) => {
+  const date = req.params.date;
+  try {
+    connection.query(
+      "SELECT * "+
+      "FROM tbl_salary s "+
+      "LEFT JOIN tbl_member m ON m.nationalId = s.nationalId "+
+      "LEFT JOIN tbl_member_role mr ON mr.memberRoleId = m.memberRoleId "+
+      "LEFT JOIN tbl_member_type mt ON mt.memberTypeId = m.memberTypeId "+
+      "LEFT JOIN tbl_payment_type pt ON pt.paymentTypeId = m.paymentTypeId "+
+      "LEFT JOIN tbl_position p ON p.positionId = m.positionId "+
+      "WHERE salaryMonth = ? "+
+      "ORDER BY memberName ASC",
+      [date],
+      (err, results, fields) => {
+        if (err) {
+          console.log(err);
+          return res.status(400).send();
+        }
+        res.status(200).json(results);
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
 // post
 router.post(
   "/",
-  body("nationalId").custom((value, { req }) => {
+  body("date").custom((value, { req }) => {
     return new Promise((resolve, reject) => {
-      const nationalId = req.body.nationalId;
+      const date = req.body.date;
       connection.query(
-        "SELECT nationalId FROM tbl_member WHERE nationalId = ? ",
-        [nationalId],
+        "SELECT * FROM tbl_salary WHERE salaryMonth = ? ",
+        [date],
         (err, res) => {
           if (err) {
             reject(new Error("Server Error"));
           }
           if (res.length > 0) {
-            reject(new Error("nationalId is already existed!"));
+            reject(new Error("บันทึกข้อมูลบัญชีการจ่ายเงินเดือนบุคลากรซ้ำ!"));
           }
           resolve(true);
         }
@@ -120,55 +149,46 @@ router.post(
     });
   }),
   async (req, res) => {
-    const {nationalId,
-      memberName,
-      houseNo,
-      streetName,
-      villageName,
-      villageNo,
-      subDistrict,
-      district,
-      province,
-      postCode,
-      contactNo,
-      positionId,
-      salary,
-      paymentTypeId,
-      memberTypeId,
-      memberRoleId,
-      memberStatusId,
-      shareQuantity, 
-      valuePerShare, 
-      totalShare,
+    const {date,
+      healthInsurance,
       username} = req.body;
+      console.log(req.body);
   const datetime =  moment().format('YYYY-MM-DD H:m:s');
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
-        errors: errors.array(),
+         status: "error", message: "บันทึกข้อมูลบัญชีการจ่ายเงินเดือนบุคลากรซ้ำ!" 
       });
     }
     try {
       connection.query(
-        "INSERT INTO tbl_member(memberName, houseNo, streetName, villageName, villageNo, "+
-        "subDistrict, district, province,postCode, contactNo, positionId, "+
-        "salary, paymentTypeId,memberTypeId, memberRoleId, memberStatusId, nationalId) "+
-        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-        [memberName, houseNo, streetName, villageName, villageNo, subDistrict, district, province,postCode, contactNo, positionId,
-          salary, paymentTypeId,memberTypeId, memberRoleId, memberStatusId, nationalId],
+        "SELECT * "+
+      "FROM tbl_member m "+
+      "LEFT JOIN tbl_member_role mr ON mr.memberRoleId = m.memberRoleId "+
+      "LEFT JOIN tbl_member_type mt ON mt.memberTypeId = m.memberTypeId "+
+      "LEFT JOIN tbl_payment_type pt ON pt.paymentTypeId = m.paymentTypeId "+
+      "LEFT JOIN tbl_position p ON p.positionId = m.positionId "+
+      "WHERE m.memberStatusId = 1 "+
+      "AND m.paymentTypeId = 2 "+
+      // "AND isHealthInsurance = 1 "+
+      // "AND salaryMonth = ? "+
+      "ORDER BY memberName ASC ",
         (err, results, fields) => {
           if (err) {
-            console.log("Error while inserting a member into database!", err);
+            console.log("บันทึกข้อมูลเงินเดือนล้มเหลว", err);
             return res.status(400).send();
           }
-          connection.query("INSERT INTO tbl_spouse(memberNationalId) VALUES (?)", nationalId);
-          connection.query(
-            "INSERT INTO tbl_investment(investmentTypeId, shareQuantity, valuePerShare, totalShare, nationalId, investmentDateTime, createdAt, createdBy, investmentStatusId, approvedAt, approvedBy) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
-            [1, shareQuantity, valuePerShare, totalShare, nationalId, datetime, datetime, username, 1, datetime, username]);
+          for(i=0; i<results.length; i++){
+          
+            connection.query("INSERT INTO tbl_salary (salaryMonth, nationalId, basedSalary, healthInsurancePercentage, healthInsurance, netSalary) VALUES (?, ?, ?, ?, ?, ?)", 
+            [date, results[i]['nationalId'], results[i]['salary'], healthInsurance, (healthInsurance * results[i]['salary']), results[i]['salary'] - (healthInsurance * results[i]['salary'])]);
+
+          }
+
           return res
             .status(201)
-            .json({ status: "success", message: "บันทึกการเพิ่มสมาชิกใหม่เรียบร้อยแล้ว!" });
+            .json({ status: "success", message: "บันทึกข้อมูลเงินเดือนเรียบร้อยแล้ว!" });
         }
       );
     } catch (err) {
