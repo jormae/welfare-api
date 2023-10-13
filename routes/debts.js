e41140a0979412b37e06a2d448ad0fbcaff156b1
Maheedeen Jormae <maheedeen@gmail.com>
2023-10-13 10:40:00 +0700

    add salary

--- routes/debts.js
+++ routes/debts.js
@@ -16,18 +16,29 @@ router.get("/date/:yearMonth", (req, res) => {
     const yearMonthDate = req.params.yearMonth + "-01"
   try {
     const mysql =
-      "SELECT m.nationalId, memberName, "+
-      "(SELECT bank FROM tbl_debt d WHERE d.nationalId = m.nationalId AND yearMonth = ?) AS bank, (SELECT allowance FROM tbl_debt d WHERE d.nationalId = m.nationalId AND yearMonth = ?) AS allowance,  "+
-      "(SELECT studentLoan FROM tbl_debt d WHERE d.nationalId = m.nationalId AND yearMonth = ?) AS studentLoan,  "+
-      "(SELECT houseRent FROM tbl_debt d WHERE d.nationalId = m.nationalId AND yearMonth = ?) AS houseRent,  "+
-      "(SELECT TIMESTAMPDIFF(MONTH, startLoanDate, ?) + 1 FROM tbl_loan ll LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND loanMainTypeId = 1 ) AS totalLoanDuration1, "+
-      "(SELECT COUNT(*) FROM tbl_loan_payment lp  LEFT JOIN tbl_loan ll ON ll.loanId = lp.loanId LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND loanMainTypeId = 1) AS totalPaymentMonth1, "+
-      "(SELECT TIMESTAMPDIFF(MONTH, startLoanDate, ?) + 1 FROM tbl_loan ll LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND loanMainTypeId = 2 ) AS totalLoanDuration2, "+
-      "(SELECT COUNT(*) FROM tbl_loan_payment lp LEFT JOIN tbl_loan ll ON ll.loanId = lp.loanId LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND lt.loanMainTypeId = 2) AS totalPaymentMonth2  "+
+    "SELECT nationalId, memberName, debtId, bank, allowance, studentLoan, houseRent, "+
+    "totalLoanDuration1, totalPaymentMonth1, monthlyPayment1, ((totalLoanDuration1 - totalPaymentMonth1) * monthlyPayment1) AS debt1, "+
+    "totalLoanDuration2, totalPaymentMonth2, monthlyPayment2, ((totalLoanDuration2 - totalPaymentMonth2) * monthlyPayment2) AS debt2 "+
+    "FROM "+
+    "( "+
+      "SELECT m.nationalId, memberName, debtId, "+
+        "(SELECT bank FROM tbl_debt d WHERE d.nationalId = m.nationalId AND yearMonth = ?) AS bank, (SELECT allowance FROM tbl_debt d WHERE d.nationalId = m.nationalId AND yearMonth = ?) AS allowance,  "+
+        "(SELECT studentLoan FROM tbl_debt d WHERE d.nationalId = m.nationalId AND yearMonth = ?) AS studentLoan,  "+
+        "(SELECT houseRent FROM tbl_debt d WHERE d.nationalId = m.nationalId AND yearMonth = ?) AS houseRent,  "+
+        "(SELECT TIMESTAMPDIFF(MONTH, CONCAT(SUBSTR(startLoanDate,1,7),'-01'), ?) + 1 FROM tbl_loan ll LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND loanMainTypeId = 1 ) AS totalLoanDuration1, "+
+        "(SELECT COUNT(*) FROM tbl_loan_payment lp LEFT JOIN tbl_loan ll ON ll.loanId = lp.loanId LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND loanMainTypeId = 1 AND EXTRACT(YEAR_MONTH FROM loanPaymentMonth) <= EXTRACT(YEAR_MONTH FROM ?)) AS totalPaymentMonth1, "+
+        "(SELECT monthlyPayment FROM tbl_loan ll LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND loanMainTypeId = 1) AS monthlyPayment1, "+
+        "(SELECT TIMESTAMPDIFF(MONTH, CONCAT(SUBSTR(startLoanDate,1,7),'-01'), ?) + 1 FROM tbl_loan ll LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND loanMainTypeId = 2 ) AS totalLoanDuration2, "+
+        "(SELECT COUNT(*) FROM tbl_loan_payment lp LEFT JOIN tbl_loan ll ON ll.loanId = lp.loanId LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND lt.loanMainTypeId = 2 AND EXTRACT(YEAR_MONTH FROM loanPaymentMonth) <= EXTRACT(YEAR_MONTH FROM ?)) AS totalPaymentMonth2,  "+
+        "(SELECT monthlyPayment FROM tbl_loan ll LEFT JOIN tbl_loan_type lt ON lt.loanTypeId = ll.loanTypeId WHERE ll.nationalId = m.nationalId AND closeLoanStatusId = 0 AND loanMainTypeId = 2) AS monthlyPayment2 "+
         "FROM tbl_member m "+
-        "WHERE (m.nationalId IN ( SELECT DISTINCT nationalId FROM tbl_loan ) OR m.nationalId IN (SELECT DISTINCT nationalId FROM tbl_debt )) "+
+        "LEFT JOIN tbl_debt d ON d.nationalId = m.nationalId "+
+            "WHERE (m.nationalId IN ( SELECT DISTINCT nationalId FROM tbl_loan WHERE closeLoanStatusId = 0) OR m.nationalId IN (SELECT DISTINCT nationalId FROM tbl_debt )) "+
+        ") AS x "+
+        "HAVING (debt1 <> 0 OR debt2 <> 0 OR bank IS NOT NULL OR allowance IS NOT NULL OR studentLoan IS NOT NULL OR houseRent IS NOT NULL) "+
         "ORDER BY memberName";
-    connection.query(mysql, [yearMonth, yearMonth, yearMonth, yearMonth, yearMonthDate, yearMonthDate], (err, results, fields) => {
+        // console.log(mysql)
+    connection.query(mysql, [yearMonth, yearMonth, yearMonth, yearMonth, yearMonthDate, yearMonthDate, yearMonthDate, yearMonthDate], (err, results, fields) => {
       if (err) {
         console.log(err);
         return res.status(400).send();
@@ -44,11 +55,13 @@ router.get("/sum/:yearMonth", (req, res) => {
     const yearMonth = req.params.yearMonth
   try {
     const mysql =
-      "SELECT SUM(debtAmount) AS TOTAL_ALLOWANCE_BALANCE, "+ 
-      "(SELECT SUM(debtAmount) FROM tbl_debt al WHERE debtTypeId = 1) AS INCOME_ALLOWANCE,  "+
-      "(SELECT SUM(debtAmount) FROM tbl_debt al WHERE debtTypeId = 2) AS EXPENSE_ALLOWANCE  "+
-      "FROM tbl_debt a ";
-    connection.query(mysql, (err, results, fields) => {
+      "SELECT SUM(bank) AS TOTAL_BANK_DEBT, "+
+      "SUM(allowance) AS TOTAL_ALLOWANCE_DEBT, "+
+      "SUM(studentLoan) AS TOTAL_STUDENTLOAN_DEBT,  "+
+      "SUM(houseRent) AS TOTAL_HOUSERENT_DEBT  "+
+      "FROM tbl_debt d "+
+      "WHERE yearMonth = ?";
+    connection.query(mysql, yearMonth, (err, results, fields) => {
       if (err) {
         console.log(err);
         return res.status(400).send();
---
+++ routes/salaries.js
@@ -0,0 +1,224 @@
+const express = require("express");
+const connection = require("../config/database-connection");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const { body, validationResult } = require("express-validator");
+const { query } = require("express");
+const moment = require("moment-timezone");
+moment.tz.setDefault("Asia/Bangkok");
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.get("/", (req, res) => {
+  try {
+    const mysql = "SELECT *, SUM(totalShare) AS TOTAL_SHARE "+
+    "FROM tbl_member m  "+
+    "LEFT JOIN tbl_member_role mr ON mr.memberRoleId = m.memberRoleId  "+
+    "LEFT JOIN tbl_member_type mt ON mt.memberTypeId = m.memberTypeId  "+
+    "LEFT JOIN tbl_payment_type pt ON pt.paymentTypeId = m.paymentTypeId  "+
+    "LEFT JOIN tbl_position p ON p.positionId = m.positionId  "+
+    "LEFT JOIN tbl_spouse s ON s.memberNationalId = m.nationalId "+ 
+    "LEFT JOIN tbl_investment i ON i.nationalId = m.nationalId  "+
+    "LEFT JOIN tbl_member_status ms ON ms.memberStatusId = m.memberStatusId "+
+    "GROUP BY m.nationalId  "+
+    "ORDER BY memberName";
+    connection.query(
+      mysql, (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).send();
+      }
+      res.status(200).json(results);
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+}
+);
+
+router.get("/:nationalId", async (req, res) => {
+  const nationalId = req.params.nationalId;
+  try {
+    connection.query(
+      "SELECT * "+
+      "FROM tbl_member m "+
+      "LEFT JOIN tbl_member_role mr ON mr.memberRoleId = m.memberRoleId "+
+      "LEFT JOIN tbl_member_type mt ON mt.memberTypeId = m.memberTypeId "+
+      "LEFT JOIN tbl_payment_type pt ON pt.paymentTypeId = m.paymentTypeId "+
+      "LEFT JOIN tbl_position p ON p.positionId = m.positionId "+
+      "LEFT JOIN tbl_spouse s ON s.memberNationalId = m.nationalId "+
+      "WHERE m.nationalId = ?",
+      [nationalId],
+      (err, results, fields) => {
+        if (err) {
+          console.log(err);
+          return res.status(400).send();
+        }
+        res.status(200).json(results);
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
+router.get("/date/:date", async (req, res) => {
+  const date = req.params.date;
+//   const healthInsurance = req.params.healthInsurance;
+  try {
+    connection.query(
+      "SELECT * "+
+      "FROM tbl_member m "+
+      "LEFT JOIN tbl_member_role mr ON mr.memberRoleId = m.memberRoleId "+
+      "LEFT JOIN tbl_member_type mt ON mt.memberTypeId = m.memberTypeId "+
+      "LEFT JOIN tbl_payment_type pt ON pt.paymentTypeId = m.paymentTypeId "+
+      "LEFT JOIN tbl_position p ON p.positionId = m.positionId "+
+      // "LEFT JOIN tbl_salary s ON s.nationalId = m.nationalId "+
+      "WHERE m.memberStatusId = 1 "+
+      "AND m.paymentTypeId = 2 "+
+      // "AND isHealthInsurance = 1 "+
+      // "AND salaryMonth = ? "+
+      "ORDER BY memberName ASC",
+      // [date],
+      (err, results, fields) => {
+        if (err) {
+          console.log(err);
+          return res.status(400).send();
+        }
+        res.status(200).json(results);
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+// post
+router.post(
+  "/",
+  body("nationalId").custom((value, { req }) => {
+    return new Promise((resolve, reject) => {
+      const nationalId = req.body.nationalId;
+      connection.query(
+        "SELECT nationalId FROM tbl_member WHERE nationalId = ? ",
+        [nationalId],
+        (err, res) => {
+          if (err) {
+            reject(new Error("Server Error"));
+          }
+          if (res.length > 0) {
+            reject(new Error("nationalId is already existed!"));
+          }
+          resolve(true);
+        }
+      );
+    });
+  }),
+  async (req, res) => {
+    const {nationalId,
+      memberName,
+      houseNo,
+      streetName,
+      villageName,
+      villageNo,
+      subDistrict,
+      district,
+      province,
+      postCode,
+      contactNo,
+      positionId,
+      salary,
+      paymentTypeId,
+      memberTypeId,
+      memberRoleId,
+      memberStatusId,
+      shareQuantity, 
+      valuePerShare, 
+      totalShare,
+      username} = req.body;
+  const datetime =  moment().format('YYYY-MM-DD H:m:s');
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    try {
+      connection.query(
+        "INSERT INTO tbl_member(memberName, houseNo, streetName, villageName, villageNo, "+
+        "subDistrict, district, province,postCode, contactNo, positionId, "+
+        "salary, paymentTypeId,memberTypeId, memberRoleId, memberStatusId, nationalId) "+
+        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        [memberName, houseNo, streetName, villageName, villageNo, subDistrict, district, province,postCode, contactNo, positionId,
+          salary, paymentTypeId,memberTypeId, memberRoleId, memberStatusId, nationalId],
+        (err, results, fields) => {
+          if (err) {
+            console.log("Error while inserting a member into database!", err);
+            return res.status(400).send();
+          }
+          connection.query("INSERT INTO tbl_spouse(memberNationalId) VALUES (?)", nationalId);
+          connection.query(
+            "INSERT INTO tbl_investment(investmentTypeId, shareQuantity, valuePerShare, totalShare, nationalId, investmentDateTime, createdAt, createdBy, investmentStatusId, approvedAt, approvedBy) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+            [1, shareQuantity, valuePerShare, totalShare, nationalId, datetime, datetime, username, 1, datetime, username]);
+          return res
+            .status(201)
+            .json({ status: "success", message: "บันทึกการเพิ่มสมาชิกใหม่เรียบร้อยแล้ว!" });
+        }
+      );
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send();
+    }
+  }
+);
+
+router.put("/:nationalId", async (req, res) => {
+  const {
+   memberName,
+   houseNo,
+   streetName,
+   villageName,
+   villageNo,
+   subDistrict,
+   district,
+   province,
+   postCode,
+   contactNo,
+   positionId,
+   salary,
+   paymentTypeId,
+   memberTypeId,
+   memberRoleId,
+   memberStatusId,
+   resignDate } = req.body;
+   const nationalId = req.params.nationalId
+  try {
+    connection.query(
+      "UPDATE tbl_member SET memberName = ?, houseNo = ?, streetName = ?, villageName = ?, villageNo = ?, subDistrict = ?, "+
+      "district = ?, province = ?, postCode = ?, contactNo = ?, positionId = ?, salary = ?, paymentTypeId = ?, memberTypeId = ?, "+
+      "memberRoleId = ?, memberStatusId = ?, resignDate = ? WHERE nationalId = ? ",
+      [memberName, houseNo, streetName, villageName, villageNo, subDistrict, district, province,postCode, contactNo, positionId,
+        salary, paymentTypeId,memberTypeId, memberRoleId, memberStatusId, resignDate, nationalId],
+      (err, results, fields) => {
+        if (err) {
+          console.log("Error while updating a member in database!", err);
+          return res.status(400).send();
+        }
+        return res
+          .status(200)
+          .json({status:'success', message: "บันทึกข้อมูลสมาชิกสำเร็จ!" });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
+module.exports = router;
